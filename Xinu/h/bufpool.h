feef2f7b3a1bed02a635d87ea3973b50b785aded
Jon Mathews <jon@ensoftcorp.com>
2017-02-23 18:02:05 -0600

    Add missing semi-colon in struct bpool

--- Xinu/h/bufpool.h
+++ Xinu/h/bufpool.h
@@ -13,7 +13,7 @@
 struct	bpool	{			/* Description of a single pool	*/
 	int	bpsize;			/* size of buffers in this pool	*/
 	char	*bpnext;		/* pointer to next free buffer	*/
-	int	bpsem			/* semaphore that counts buffers*/
+	int	bpsem;			/* semaphore that counts buffers*/
 	};				/*  currently in THIS pool	*/
 
 extern	struct	bpool bptab[];		/* Buffer pool table		*/
